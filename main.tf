a25fb11b8c888eb916c0b8f950618c046c521ff4
Ramparitala <ramparitala7@gmail.com>
2022-08-19 13:55:20 +0100

    Add sbox as an environmnet to pickup repo

--- main.tf
+++ main.tf
@@ -11,3 +11,10 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
   tags     = var.common_tags
 }
+# sandbox environmnet
+provider "azurerm" {
+  subscription_id = "a8140a9e-f1b0-481f-a4de-09e2ee23f7ab"
+  alias           = "sds-sbox"
+  features {}
+  
+}
